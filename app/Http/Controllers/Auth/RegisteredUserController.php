f344043c7f1b49ce8ae97b303d52ab8783b64183
Pale Kiwi <contact@palekiwi.com>
2023-09-10 19:36:45 +0800

    finish 03

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,7 +32,7 @@ class RegisteredUserController extends Controller
     public function store(Request $request): RedirectResponse
     {
         $request->validate([
-            'username' => 'required|string|max:255|unique:'.User::class,
+            'username' => 'required|string|max:20|unique:'.User::class,
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:'.User::class,
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
---
+++ volar.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    services: [require("volar-service-prettyhtml").create({ printWidth: 80 })]
+};
