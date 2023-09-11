03de612e1d5fe6fca1bb040f7dbdd0ef9dd532d1
Pale Kiwi <contact@palekiwi.com>
2023-09-11 12:01:22 +0800

    finish 04

--- resources/js/Pages/Forum/Index.vue
+++ resources/js/Pages/Forum/Index.vue
@@ -1,18 +1,20 @@
 <script setup>
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
+import ForumLayout from "@/Layouts/ForumLayout.vue";
+import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <AuthenticatedLayout>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">Index</div>
-                </div>
-            </div>
+    <ForumLayout>
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">Forum Index</div>
         </div>
-    </AuthenticatedLayout>
+
+        <template #side>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">Side</div>
+            </div>
+        </template>
+    </ForumLayout>
 </template>
