02055aa8ef473ab5ee8c635ae59740c0d3945bfd
Pale Kiwi <contact@palekiwi.com>
2023-09-13 12:11:29 +0800

    update pod.sh

--- pod.sh
+++ pod.sh
@@ -1,16 +1,17 @@
 POD_NAME=laravel-forum
 CMD=$1
 
-if [ -z $CMD ]; then
-    echo "Command must be start or stop"
-    exit 1;
-elif [ $CMD = "start" ]; then
-    echo "Starting pod $POD_NAME..."
-    podman pod start $POD_NAME
-elif [ $CMD = "stop" ]; then
-    echo "Stoping pod $POD_NAME..."
-    podman pod start $POD_NAME
-else
-    echo "Unknown command: $CMD"
-    exit 1;
-fi
+case $CMD in
+    "start")
+        echo "Starting pod $POD_NAME..."
+        podman pod start $POD_NAME
+        ;;
+    "stop")
+        echo "Stopping pod $POD_NAME..."
+        podman pod stop $POD_NAME
+        ;;
+    *)
+        echo "Invalid command. Must be start or stop."
+        exit 1;
+        ;;
+esac
