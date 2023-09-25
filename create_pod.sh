9c3ce9cd646724a3d2603b8f68ed9548bf889e9a
Pale Kiwi <contact@palekiwi.com>
2023-09-25 11:33:56 +0800

    update create_pod

--- create_pod.sh
+++ create_pod.sh
@@ -1,9 +1,9 @@
 POD_NAME=laravel-forum
 
 APP_IMAGE=localhost/laravel/8.2-pgsql
-PGADMIN_IMAGE=docker.io/dpage/pgadmin4
+PGADMIN_IMAGE=docker.io/dpage/pgadmin4:latest
 PGSQL_IMAGE=docker.io/library/postgres
-SEARCH_IMAGE=docker.io/getmeili/meilisearch:latest
+SEARCH_IMAGE=docker.io/getmeili/meilisearch:v1.3
 
 APP_DIR=/home/pl/code/php/laravel-forum
 DB_DIR=/home/pl/box/laravel-forum/db
@@ -11,10 +11,16 @@ SEARCH_DIR=/home/pl/box/laravel-forum/meili_data
 
 PGSQL_PASSWORD=1234
 
+MEILI_MASTER_KEY=rgzUyt-OYHJWzjoojcTDeLE4waAmsK67mdH6AYXS93k
+
 PGADMIN_EMAIL=pgadmin@example.com
 PGADMIN_PASSWORD=K4HeAk1xzNg4OZ9zAZVKSSwXs
 PGADMIN_PORT=5050
 
+mkdir -p $DB_DIR
+mkdir -p $SEARCH_DIR
+
+# create pod
 podman pod create \
     -p 8000:8000 \
     -p 5050:5050 \
@@ -27,15 +33,16 @@ podman pod create \
 # app container
 podman create \
     --name ${POD_NAME}-app \
-    -v $APP_DIR:/app:Z \
+    -v ${APP_DIR}:/app:Z \
     --pod $POD_NAME \
     $APP_IMAGE
 
 # db container
 podman create \
     --name ${POD_NAME}-db \
-    -e "POSTGRES_PASSWORD=$PGSQL_PASSWORD" \
-    -v $DB_DIR:/var/lib/postgresql/data:Z \
+    -e "POSTGRES_PASSWORD=${PGSQL_PASSWORD}" \
+    -v ${DB_DIR}:/var/lib/postgresql/data:Z \
+    -v $(pwd)/database/sql/initdb.d:/docker-entrypoint-initdb.d:Z \
     --pod $POD_NAME \
     $PGSQL_IMAGE
 
@@ -44,15 +51,16 @@ podman create \
     --name ${POD_NAME}-search \
     --pod $POD_NAME \
     -e MEILI_ENV="development" \
-    -e MEILI_MASTER_KEY='rgzUyt-OYHJWzjoojcTDeLE4waAmsK67mdH6AYXS93k'\
-    -v $SEARCH_DIR:/meili_data:Z \
+    -e MEILI_MASTER_KEY=${MEILI_MASTER_KEY}\
+    -v ${SEARCH_DIR}:/meili_data:Z \
     $SEARCH_IMAGE
 
 # pgadmin container
 podman create \
-    -e "PGADMIN_DEFAULT_EMAIL=$PGADMIN_EMAIL" \
-    -e "PGADMIN_DEFAULT_PASSWORD=$PGADMIN_PASSWORD" \
-    -e "PGADMIN_LISTEN_PORT=$PGADMIN_PORT" \
+    -e "PGADMIN_DEFAULT_EMAIL=${PGADMIN_EMAIL}" \
+    -e "PGADMIN_DEFAULT_PASSWORD=${PGADMIN_PASSWORD}" \
+    -e "PGADMIN_LISTEN_PORT=${PGADMIN_PORT}" \
+    -v $(pwd)/database/pgadmin/servers.json:/pgadmin4/servers.json:Z \
     --name ${POD_NAME}-pgadmin \
     --pod $POD_NAME \
     $PGADMIN_IMAGE
